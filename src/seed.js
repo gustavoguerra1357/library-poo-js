12d50fc23dba0c58c1e8a146e0128e2789b45486
gustavoguerradev <gustavoguerra852@gmail.com>
2026-02-11 22:46:14 -0300

    GitHub Pages config

--- src/seed.js
+++ src/seed.js
@@ -5,49 +5,49 @@ export const seedBooks = [
     "1984", 
     "George Orwell",
     328, 
-    "./public/covers/1984.jpg",
+    "./covers/1984.jpg",
     32
   ),
   new Book(
     "A Revolução dos Bichos", 
     "George Orwell",
     152, 
-    "./public/covers/revolucao.jpg",
+    "./covers/revolucao.jpg",
     3
   ),
   new Book(
     "Eu, Robô",
     "Isaac Asimov", 
     320, 
-    "./public/covers/eu-robo.jpg",
+    "./covers/eu-robo.jpg",
     7
   ),
   new Book(
     "O Alienista", 
     "Machado de Assis",
     160, 
-    "./public/covers/alienista.jpg",
+    "./covers/alienista.jpg",
     11
   ),
   new Book(
     "Sherlock Holmes - Um Estudo em Vermelho", 
     "Arthur Conan Doyle",
     160, 
-    "./public/covers/sherlock.jpg",
+    "./covers/sherlock.jpg",
     16
   ),
   new Book(
     "Frankenstein", 
     "Mary Shelley",
     304, 
-    "./public/covers/frankenstein.jpg",
+    "./covers/frankenstein.jpg",
     9
   ),
   new Book(
     "Fahrenheit 451", 
     "Ray Bradbury",
     216, 
-    "./public/covers/fahrenheit.jpg",
+    "./covers/fahrenheit.jpg",
     7
   )
 ];
